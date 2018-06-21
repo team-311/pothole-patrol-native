90ecf92ab14e90742c0690e049487edde57a1ee4
Cody Heslep <codyheslep@gmail.com>
2018-06-21 14:34:10 -0500

    added bottom nav bar

--- components/ReportPhoto.js
+++ components/ReportPhoto.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 
-export default class Report1 extends React.Component {
+export default class ReportPhoto extends React.Component {
   render() {
     return (
       <View style={styles.container}>
