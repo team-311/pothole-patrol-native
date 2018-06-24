b5bc0e6702ee69254bc8dda98da021938d92885e
chawes <hawes.conor@gmail.com>
2018-06-24 13:34:43 -0500

    Improved styling for final step of form submission

--- components/ReportForm.js
+++ components/ReportForm.js
@@ -1,13 +1,6 @@
 import React, { Component } from 'react';
-import {
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-  Picker,
-  Switch,
-  TouchableHighlight,
-} from 'react-native';
+import { StyleSheet, Text, View, Switch } from 'react-native';
+import { Container, Content, Form, Textarea, Picker, Button, Icon } from 'native-base'
 import {NavigationActions, StackActions} from 'react-navigation'
 
 export default class ReportForm extends Component {
@@ -28,78 +21,79 @@ export default class ReportForm extends Component {
       actions: [NavigationActions.navigate({routeName: 'Base'})]
     })
     this.props.navigation.dispatch(action)
-  };
+  }
 
   render() {
     return (
-      <View style={styles.container}>
-        <TextInput
-          style={styles.description}
-          autoFocus
-          multiline
-          placeholder="Enter pothole description"
-          maxLength={500}
-          value={this.state.description}
-          onChangeText={text => this.setState({ description: text })}
-        />
-        <Text>Where is the pothole located?</Text>
-        <Picker
-          selectedValue={this.state.placement}
-          onValueChange={itemValue => this.setState({ placement: itemValue })}
-          mode="dropdown"
-          prompt="Where is the pothole located?"
-        >
-          <Picker.Item label="--Select an option--" value="" />
-          <Picker.Item label="Bike Lane" value="Bike Lane" />
-          <Picker.Item label="Crosswalk" value="Crosswalk" />
-          <Picker.Item label="Curb Lane" value="Curb Lane" />
-          <Picker.Item label="Intersection" value="Intersection" />
-          <Picker.Item label="Traffic Lane" value="Traffic Lane" />
-        </Picker>
-        <View style={styles.switchGroup}>
-          <Switch
-            onValueChange={value => this.setState({ anonymous: value })}
-            value={this.state.anonymous}
-          />
-          <Text style={{ marginLeft: 10 }}>Remain Anonymous?</Text>
-        </View>
-        <TouchableHighlight
-          style={styles.button}
-          accessibilityLabel="Submit pothole service request"
-          onPress={this.handleSubmit}
-          disabled={!this.state.placement}
-        >
-          <Text style={styles.buttonText}>Submit</Text>
-        </TouchableHighlight>
-      </View>
-    );
+      <Container>
+        <Content>
+          <Form>
+            <View style={styles.formGroup}>
+              <Textarea style={styles.description}
+                rowSpan={5}
+                placeholder='Provide additional information (if necessary)'
+                bordered
+                onChangeText={text => this.setState({ description: text })}
+                maxLength={500}
+                value={this.state.description}
+                autoFocus
+              />
+            </View>
+            <View style={styles.formGroup}>
+              <Picker
+                mode="dropdown"
+                placeholder="Select pothole location"
+                placeholderStyle={{ color: "lightgray" }}
+                iosIcon={<Icon name="ios-arrow-down-outline" />}
+                selectedValue={this.state.placement}
+                onValueChange={text => this.setState({ placement: text })}
+              >
+                <Picker.Item label="Bike Lane" value="Bike Lane" />
+                <Picker.Item label="Crosswalk" value="Crosswalk" />
+                <Picker.Item label="Curb Lane" value="Curb Lane" />
+                <Picker.Item label="Intersection" value="Intersection" />
+                <Picker.Item label="Traffic Lane" value="Traffic Lane" />
+              </Picker>
+            </View>
+            <View style={styles.userDetails}>
+              <Switch
+                onValueChange={value => this.setState({ anonymous: value })}
+                value={this.state.anonymous}
+              />
+              <Text style={{ marginLeft: 10 }}>Remain Anonymous?</Text>
+            </View>
+            <Button style={styles.button}
+              primary
+              block
+              disabled={!this.state.placement}
+              onPress={this.handleSubmit}
+            >
+              <Text style={styles.buttonText}>Submit</Text>
+            </Button>
+          </Form>
+        </Content>
+      </Container>
+    )
   }
 }
 
 const styles = StyleSheet.create({
   description: {
-    borderStyle: 'solid',
-    borderColor: 'black',
-    borderWidth: 1,
-    padding: 2,
-  },
-  container: {
-    padding: 10,
-    backgroundColor: 'white',
-  },
-  button: {
-    alignItems: 'center',
-    backgroundColor: '#B3DDF2',
-    padding: 10,
+    borderRadius: 5
   },
   buttonText: {
     fontSize: 18,
     fontWeight: 'bold',
     color: 'white',
   },
-  switchGroup: {
-    alignItems: 'center',
-    flexDirection: 'row',
+  button: {
+    marginTop: 30,
+  },
+  formGroup: {
     marginBottom: 20,
   },
-});
+  userDetails: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+})
