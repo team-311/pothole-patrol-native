d64ff3763deb9a9c18e579be22265d9345a1799f
chawes <hawes.conor@gmail.com>
2018-06-26 17:53:01 -0500

    Updating settings UI

--- components/Settings.js
+++ components/Settings.js
@@ -2,14 +2,16 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { logout } from '../store'
 import { Text } from 'react-native'
-import { Container, Button, Header, Content } from 'native-base'
+import { Container, Button, Header, Content, H1 } from 'native-base'
 
 const Settings = (props) => {
   return (
     <Container>
-      <Header />
-      <Content>
-        <Button block bordered danger onPress={props.logout}>
+      <Header style={{justifyContent: 'flex-start'}}>
+        <H1>User Settings</H1>
+      </Header>
+      <Content style={{backgroundColor: 'white'}}>
+        <Button block danger style={{width: '80%', marginTop: 40, alignSelf: 'center'}}onPress={props.logout}>
           <Text>Log out</Text>
         </Button>
       </Content>
